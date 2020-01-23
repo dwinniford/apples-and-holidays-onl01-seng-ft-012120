8b9b23f65af8cf676597bda402965d4e2b43d313
Dave Winniford <dwinniford@gmail.com>
2020-01-23 23:20:19 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
         word.capitalize
       end 
       holiday_string = holiday_name_array.join(" ")
-      puts "  #{holiday_string}: #{supply.join(", ")}"
+      puts "  #{holiday_string}: #{supply.join(', ')}"
       
     end 
   end 
